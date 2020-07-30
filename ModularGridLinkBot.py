02a8b6f1d6887469c7461957dde498d97fec13e4
Wesley Mauk <WesleyM77@users.noreply.github.com>
2020-07-29 22:32:55 -0400

    Update to the almost real bot

--- ModularGridLinkBot.py
+++ ModularGridLinkBot.py
@@ -6,15 +6,28 @@ import time
 
 start = time.time()
 
+# The bot config to use from praw.ini
 reddit = praw.Reddit('bot1')
-subreddit = reddit.subreddit("magictcg+mtgjudge+mtgaltered+MTGO+mtgcube+mtgfinance+lrcast+CompetitiveEDH")
-ignore = str(reddit.redditor('mtgcardfetcher'))
+# The subreddits to scan
+# if multiple, text would be all one stringg with each subreddit separated by + eg: "all+test"
+subreddit = reddit.subreddit("ShadowWesley77")
+# Put the name of the bot here!
+ignore = str(reddit.redditor('ShadowWesley77'))
+# The text we are looking for and the regex to find the ID
+searchtext = "https://cdn.modulargrid.net/img/racks/modulargrid_"
+regexfindid = 'https:\/\/cdn\.modulargrid\.net\/img\/racks\/modulargrid_([0-9]+)\.jpg'
 
-lmsrcomment = """>Queen Marchesa (long may she reign)\n
-                    \nPlease address the Queen with respect. I'm a bot. If I've made a mistake, click [here.]
+# The two parts of the comment
+botcomment = """You posted a link to your ModularGrid rack image. The link to the actual rack is more helpful in
+                    providing advice as it allows users to see a module's function without having seen it before.
+                    \n\nHere's the link to the actual rack: https://www.modulargrid.net/e/racks/view/"""
+
+botcommentpart2 = """
+    \n\nI'm a bot. If I've made a mistake, click [here.]
                     (https://www.reddit.com/message/compose?to=shadowwesley77)
-                    """
-        
+"""
+
+# Open our comments/posts replied to text files or create them if they don't exist
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
 else:
@@ -31,74 +44,62 @@ else:
        comments_replied_to = comments_replied_to.split("\n")
        comments_replied_to = list(filter(None, comments_replied_to))
 
+# some vars to help report and switch behavior
 checked = 0
 replied = 0
 switch = False
+sticky = False
 
-#Check submission titles
+# Our main loop!
 should_restart = True
 while should_restart:
     should_restart = False
+    # Switch between the hot, new, and stickied posts to scan
     if switch == True:
         posts = subreddit.new(limit=100)
         switch = False
     else:
-        posts = subreddit.hot(limit=100)
-        switch = True
+        if sticky == True:
+            posts = (subreddit.sticky(1), subreddit.sticky(2))
+            sticky = False
+        else:
+            posts = subreddit.hot(limit=100)
+            switch = True
+            sticky = True
+
+    # Check the submission text and each comment for the links
     for submission in posts:
-        if submission.id not in posts_replied_to:
-            checked = checked + 1
-            print ("Checked ", checked ," posts. ", submission.title)
-            if checked % 3500 == 0:
-                msg = "Uptime: " + str("{:.2f}".format((time.time()-start)/3600)) + " hours.\n\n Checked " + str(checked) + " posts.\n\nReplied to " + str(replied) + " posts."
-                reddit.redditor('shadowwesley77').message("Bot Status", msg,)
-                replied = 0
-            if checked % 100 == 0:
-                should_restart = True
-                break
-            if re.search("Queen Marchesa", submission.title, re.IGNORECASE) and not re.search("long may she reign", submission.title, re.IGNORECASE):
-                submission.reply(lmsrcomment)
+        checked = checked + 1
+        print ("Checked ", checked ," posts. ", submission.title)
+        if checked % 3500 == 0:
+           replied = 0
+        if checked % 100 == 0:
+           should_restart = True
+           break
+        #Check submission text
+        if re.search(searchtext, submission.selftext, re.IGNORECASE) and submission.id not in posts_replied_to:
+                mgid = re.search(regexfindid, submission.selftext)
+                submission.reply(botcomment + mgid.group(1) + botcommentpart2)
                 replied = replied + 1
                 print("Bot replied to: ", submission.title)
                 posts_replied_to.append(submission.id)
                 with open("posts_replied_to.txt", "w") as f:
                     for post_id in posts_replied_to:
                         f.write(post_id + "\n")
-            #Check comments of post
-            submission.comments.replace_more(limit=0)
-            comments = submission.comments[:]
-            while comments:
-                comment = comments.pop(0)
-                if comment.id not in comments_replied_to and str.lower(str(comment.author)) != ignore:
-                    if re.search("Queen Marchesa", comment.body, re.IGNORECASE) and not re.search("long may she reign", comment.body, re.IGNORECASE):
-                        comment.reply(lmsrcomment)
-                        replied = replied + 1
-                        print("Bot replied to comment under: ", submission.title)
-                        comments_replied_to.append(comment.id)
-                        with open("comments_replied_to.txt", "w") as t:
-                            for post_id in comments_replied_to:
-                                t.write(post_id + "\n")
-                    comments.extend(comment.replies)
-        else:
-            #Check comments of post
-            submission.comments.replace_more(limit=0)
-            comments = submission.comments[:]
-            while comments:
-                comment = comments.pop(0)
-                if comment.id not in comments_replied_to and str.lower(str(comment.author)) != ignore:
-                    if re.search("Queen Marchesa", comment.body, re.IGNORECASE) and not re.search("long may she reign", comment.body, re.IGNORECASE):
-                        comment.reply(lmsrcomment)
-                        replied = replied + 1
-                        print("Bot replied to comment under: ", submission.title)
-                        comments_replied_to.append(comment.id)
-                        with open("comments_replied_to.txt", "w") as t:
-                            for post_id in comments_replied_to:
-                                t.write(post_id + "\n")
-                    comments.extend(comment.replies)
+        #Check comments of post
+        submission.comments.replace_more(limit=0)
+        comments = submission.comments[:]
+        while comments:
+           comment = comments.pop(0)
+           if comment.id not in comments_replied_to and str.lower(str(comment.author)) != ignore:
+               if re.search(searchtext, comment.body, re.IGNORECASE):
+                   mgid = re.search(regexfindid, comment.body)
+                   comment.reply(botcomment + mgid.group(1) + botcommentpart2)
+                   replied = replied + 1
+                   print("Bot replied to comment under: ", submission.title)
+                   comments_replied_to.append(comment.id)
+                   with open("comments_replied_to.txt", "w") as t:
+                       for post_id in comments_replied_to:
+                           t.write(post_id + "\n")
+               comments.extend(comment.replies)
     should_restart = True
-
-
-
-
-            
-            
